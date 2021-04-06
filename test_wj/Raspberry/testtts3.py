be29bd2bf7f3722a9b2407a2957cbdc78dfb91db
woojin <woojinny@kakao.com>
2021-04-07 01:39:47 +0900

    RPI Voice edit file
    RPI 에서 음성과 영상 파일을 수정 및 등록하였습니다.

--- test_wj/Raspberry/testtts3.py
+++ test_wj/Raspberry/testtts3.py
@@ -6,7 +6,7 @@ import vlc
 
 def speak(text):
     tts = gTTS(text=text,lang='ko')
-    filename ='voice2.mp3'
+    filename ='voice23.mp3'
     tts.save(filename)
     # playsound.playsound('/home/pi/Desktop/arduino/' + filename)
     # playsound 는 리룩스에서는 작동하지않아요.
@@ -17,5 +17,6 @@ def speak(text):
     
     print('잘 다음으로 넘어가나요?')
     # os.system('/home/pi/Desktop/arduino/' + filename)
+# speak(" 마음이 답답할 땐 청량하게! , 해당 제품은 사이다 입니다. ")
 
-speak(" 마음이 답답할 땐 청량하게! , 해당 제품은 사이다 입니다. ")
+speak(" 올해는 도시마다 다시 즐거움이 켜질거에요! 해당 제품은 코카콜라입니다. ")
