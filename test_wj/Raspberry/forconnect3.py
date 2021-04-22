a7fdf8125d31f2509210c1346588c67652d8de2a
woojin <woojinny@kakao.com>
2021-04-23 02:35:52 +0900

    서버 연결을 통한 데이터 연동 정리 & ML을 위한 데이터셋 기초
    1. 서버 연결을 통해 얻은 데이터 자료를 정리하였습니다.
    2. 이를 바탕으로 전체 RPI 프로그래밍 정리 2단계를 하였습니다.
    3. 실제 시현 테스트를 통해 이러한 진행에 대한 파악이 필요합니다.
    4. ML을 위한 데이터 셋 구성을 위하여 CVS 파일 제작 기초를 정리 하였습니다.
    5. 더 많은 진도가 필요하지만 우선은 시간 관계 상 여기서 마무리 하겠습니다.

---
+++ test_wj/Raspberry/forconnect3.py
@@ -0,0 +1,23 @@
+with open("forconnect6.txt", "r") as f:
+    # data = list(f.read())
+    x = list(f.readlines())
+    # print("read")
+    # print(data)
+    print("readlines를 이용하였음")
+    print(x)
+
+    t = 0
+    b_name = input('검색할 음료의 이름을 서술하라\n')
+    print("줄로 나뉜 list 에서 각 요소마다 나눈다")
+    for i in range(0,len(x)):
+        x_name = list(x[i].split(','))
+        #print(x_name)
+        k = '"name":"' + b_name +'"'
+        if x_name[1] ==  k:
+            print(x_name)
+            print(" 선택하신 음료는 " + x_name[1][8:-1] + " 입니다. 또한 해당 음료의 가격은 " + x_name[4][7:] + " 원, 해당 음료의 크기는 " + x_name[4][7:] + "mm 입니다.")
+            t += 1
+        
+    if t == 0:
+        print("입력하신 음료는 없습니다.")
+    # print(data)
